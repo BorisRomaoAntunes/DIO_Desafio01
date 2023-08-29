a33125f3cf7d1779be483c5d9fbf095eb680c79e
Boris Romão Antunes <62909074+BorisRomaoAntunes@users.noreply.github.com>
2023-08-29 18:20:48 -0300

    DIO_Desafio01_DONE

---
+++ Desafio_DIO_01.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+let firstName: String = "Steve"
+var secondName: String? = "Jobs"
+
+print("O nome dele é \(firstName) \(secondName ?? "Wozniak")")
+
+if let secondName = secondName {
+    print ("O nome dele é \(firstName) \(secondName)")
+}
+
+
+/*
+ - Criar um projeto no playground usando o Xcode
+ - Definir uma _constante_ com o valor inicia "Steve"
+ - Definir uma _variável do tipos String opcional e com valor
+ inicial "Jobs"
+ - Escrever um _print_ fazendo interpolação com a _constante_
+ e _variável_, defindo um valor default para a variável
+ opcinal como "Wozniak"
+ - Fazer um Optional Binding na _variável e dento da condição
+ fazer outro _print_ com interpolação entre a constante_
+ e variável. que foi desembrulhada.
+ */
